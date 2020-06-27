acc3ed5e53520fb94bf2ab12f5c013a5ceb34214
Olayiwola <waleadejnr@gmail.com>
2020-06-27 22:22:14 +0100

    updated cloude function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,14 +1,23 @@
-exports.handler = function(event, context, callback){
- let body;
+exports.handler = function (event, context, callback) {
+  const secretContent = `<h3>Welcome to the secret area</h3>
+  <p>Here can tell you that the sky is <strong> blue</strong>, and the grass is <strong>green</strong></p>
+ `;
+  let body;
 
-if (event.body){
- body = JSON.parse(event.body)
-} else {
- body ={}
-}
- 
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
 
-if(event.body=='javascript'){
- callbacl
-}
-}
+  if (event.body == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
+};
