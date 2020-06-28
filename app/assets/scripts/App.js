cd118ea85486d9ad88b21a629d4a80503e35e0af
Olayiwola <waleadejnr@gmail.com>
2020-06-28 19:41:57 +0100

    added new images

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -9,7 +9,7 @@ new ClientArea()
 new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
-new MobileMenu();
+new MobileMenu(); 
 let modal;
 
 document.querySelectorAll(".open-modal").forEach(el => {
--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,6 @@
 exports.handler = function (event, context, callback) {
   const secretContent = `<h3>Welcome to the secret area</h3>
-  <p>Here can tell you that the sky is <strong> blue</strong>, and the grass is <strong>green</strong></p>
+  <p>Here we can tell you that the sky is <strong> blue</strong>, and the grass is <strong>green</strong></p>
  `;
   let body;
 
